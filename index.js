d0bc5180882eb65a8af796a19760771a35449ede
Lara-2109 <yasmin.lara21@escola.pr.gov.br>
2026-06-01 11:15:09 -0300

    certo

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 
 //http://localhost:3000/saudacao?nome=gisele
@@ -52,11 +54,26 @@ app.post("/media", (req, res) => {
 
     res.json({
         nota1,
-        nota2, 
+        nota2,
         mensagem: media >= 7 ? "aprovado" : "reprovado",
         media: parseFloat(media)
-      
+
     })
+})
+app.post("/login", (req, res) => {
+    const { email, senha } = req.body;
+
+    if (!email || !senha) {
+        return res.status(404).json({ erro: "Dados incompletos" })
+    }
+
+    if (email == "admin@admin.com" && senha == "123456") {
+        return res.status(200).json({ Sucesso: "Usuario logado" })
+    }
+    else( 
+        res.status(404).json({ erro: "Usuario não encontrado" }) 
+    )
+
 })
 
 
